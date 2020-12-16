177cfafef4e0dcaae765319c3b435fd68fe9e655
Kimberly <kmglock@mail.lipscomb.edu>
2020-12-15 22:26:48 -0600

    commit tree b7aa1d313dc8c69ae3bb07bffb9aae18bc471d88

--- lib/practicing_returns.rb
+++ lib/practicing_returns.rb
@@ -1,12 +1,30 @@
-require 'pry'
+# require 'pry'
+
+# def hello(array)
+#   i = 0
+#   while i < array.length
+#     yield(array[i])
+#     i += 1
+#   end
+# end
+
+# binding.pry
+# hello(["Tim", "Tom", "Jim"]) { |name| puts "Hi, #{name}" }
+
+
 
 def hello(array)
   i = 0
+  collection = []
   while i < array.length
-    yield(array[i])
+    collection << yield(array[i])
     i += 1
   end
-end
+  collection
+end 
 
 
-hello(["Tim", "Tom", "Jim"]) { |name| puts "Hi, #{name}" }
+#built our own iteration using a while loop, yielded each individual 
+#member of an array to a block and captured the return values of 
+#yielding those items to the block. This is exactly how the 
+#collect method works, and we've just built it out, all by ourselves. 
